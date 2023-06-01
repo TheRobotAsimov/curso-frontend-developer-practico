97875107b1d8058da15a9877ae3f7a5001af8369
Isaiah <th3r0b0t4s1m0v@gmail.com>
2023-06-01 11:48:07 -0600

    Add mobile menu

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 const desktop_menu = document.querySelector('.desktop-menu');
 const navbar_email = document.querySelector('.navbar-email');
+const triline_menu = document.querySelector('.menu');
+const mobile_menu = document.querySelector('.mobile-menu');
 
 navbar_email.addEventListener('click', toggleDesktopMenu);
+triline_menu.addEventListener('click', toggleMobileMenu);
 
 function toggleDesktopMenu(){
     desktop_menu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu(){
+    mobile_menu.classList.toggle('inactive');
 }
